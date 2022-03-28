f34ed6c44b3f63135151d9bfeb66fa5ffff61ce7
hongmon1 <hongmon1@naver.com>
2022-03-28 18:16:45 +0900

    splash test

--- Assets/Scripts/CameraResolution.cs
+++ Assets/Scripts/CameraResolution.cs
@@ -8,7 +8,7 @@ public class CameraResolution : MonoBehaviour
     {
         Camera camera = GetComponent<Camera>();
         Rect rect = camera.rect;
-        float scaleheight = ((float)Screen.width / Screen.height) / ((float)9 / 18); // (가로 / 세로)
+        float scaleheight = ((float)Screen.width / Screen.height) / ((float)9f / (float)19f); // (가로 / 세로)
         float scalewidth = 1f / scaleheight;
         if (scaleheight < 1)
         {
@@ -24,5 +24,5 @@ public class CameraResolution : MonoBehaviour
     }
 
     //레터박스 위를 검정색으로 칠해줌
-    void OnPreCull() => GL.Clear(true, true, Color.black);
+    //void OnPreCull() => GL.Clear(true, true, Color.black);
 }
