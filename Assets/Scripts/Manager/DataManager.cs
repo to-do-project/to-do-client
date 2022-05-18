47148ad3fd33051bd61c6b75ce1b4d2ef210b5f9
remice <qortmdwns3@gmail.com>
2022-05-18 20:19:49 +0900

    DataManager 수정, Button 이미지 추가
    DataManager에 Response<T> 클래스를 추가하였습니다.
    서버에서 받아온 Json데이터를 Response<T> 클래스로 변환하여 받고
    Response 데이터에서 isSuccess로 성공 여부 판단 후 result의 데이터 클래스(T)를 전달 받아 사용합니다.
    
    버튼 이미지> 아이템 비활성화/활성화 버튼, +/- 이미지 비활성화 버튼, 슬라이더 fill image 편집해서 추가하였습니다.

--- Assets/Scripts/Manager/DataManager.cs
+++ Assets/Scripts/Manager/DataManager.cs
@@ -24,3 +24,12 @@ public class DataManager
         return JsonUtility.FromJson<Loader>(testAsset.text);
     }
 }
+
+[System.Serializable]
+public class Response<T>    // 서버에서 받은 json파일을 FromJson<Response<T>>로 변환해 받아옵니다
+{
+    public bool isSuccess;  // 성공 여부
+    public int code;        // 오류 코드
+    public string message;  // 오류 메세지
+    public T result;        // 서버에서 받은 데이터 클래스 (실 데이터)
+}
