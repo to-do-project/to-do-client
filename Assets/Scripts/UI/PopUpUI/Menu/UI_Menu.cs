c4f3d77d4408af91a3e3e7c404c6397a69ab6a15
remice <qortmdwns3@gmail.com>
2022-05-27 16:23:01 +0900

    캐릭터 꾸미기와 테스트용 메뉴화면 추가
    토큰 발급을 위한 메뉴화면 추가
    캐릭터 꾸미기 화면 추가 및 API 연결 (미완성)

--- Assets/Scripts/UI/PopUpUI/Menu/UI_Menu.cs
+++ Assets/Scripts/UI/PopUpUI/Menu/UI_Menu.cs
@@ -2,6 +2,22 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[System.Serializable]
+public class RequestTest
+{
+    public long userId;
+    public long planetId;
+    public int planetLevel;
+    public string planetColor;
+    public string email;
+    public string nickname;
+    public long characterItem;
+    public string profileColor;
+    public int point;
+    public int missionStatus;
+    public string deviceToken;
+}
 public class UI_Menu : UI_PopupMenu
 {
     enum Buttons
@@ -14,6 +30,8 @@ public class UI_Menu : UI_PopupMenu
         Store_btn,
         Setting_btn,
         Deco_btn,
+        Refresh_btn,
+        Login_btn,
     }
 
     enum Images
@@ -45,7 +63,53 @@ public class UI_Menu : UI_PopupMenu
     {
         Bind<Button>(typeof(Buttons));
 
-        SetBtn((int)Buttons.Back_btn, ClosePopupUI);
+        SetBtn((int)Buttons.Back_btn, (data) =>
+        {
+            Response<string> res;
+
+            res = new Response<string>();
+
+            RequestSignUp val = new RequestSignUp
+            {
+                email = "tester1@gmail.com",
+                password = "test12345",
+                nickname = "tester1",
+                planetColor = "RED",
+                deviceToken = "testingtesting"
+            };
+            Managers.Web.SendPostRequest<ResponseSignUp>("join", val, (uwr) =>
+            {
+                if (res != null)
+                {
+                    res = JsonUtility.FromJson<Response<string>>(uwr.downloadHandler.text);
+
+                    if (res.isSuccess)
+                    {
+                        switch (res.code)
+                        {
+                            case 1000:
+                                Debug.Log(uwr.downloadHandler.text);
+                                Testing.instance.DeviceToken = val.deviceToken;
+                                Testing.instance.AccessToken = uwr.GetResponseHeader("Jwt-Access-Token");
+                                Testing.instance.RefreshToken = uwr.GetResponseHeader("Jwt-Refresh-Token");
+                                Debug.Log(uwr.GetResponseHeader("Jwt-Access-Token"));
+                                Debug.Log(uwr.GetResponseHeader("Jwt-Refresh-Token"));
+                                break;
+                            default:
+                                Debug.Log(res.message);
+                                break;
+                        }
+
+                    }
+                    else
+                    {
+                        Debug.Log(res.message);
+                    }
+
+                    res = null;
+                }
+            });
+        });// ClosePopupUI); // 테스트용 계정을 만들기위한 메소드. ClosePopupUI가 기존 메소드
 
         SetBtn((int)Buttons.Profile_btn, (data) => { Managers.UI.ShowPopupUI<UI_Profile>("ProfileView", $"{pathName}/Profile"); });
 
@@ -60,7 +124,62 @@ public class UI_Menu : UI_PopupMenu
         SetBtn((int)Buttons.Setting_btn, (data) => { Managers.UI.ShowPopupUI<UI_Setting>("SettingView", $"{pathName}/Setting"); });
 
         SetBtn((int)Buttons.Deco_btn, (data) => { Managers.UI.ShowPopupUI<UI_Deco>("DecoView", $"{pathName}/Deco"); });
+
+        SetBtn((int)Buttons.Refresh_btn, (data) => {
+            List<string> hN = new List<string>();
+            List<string> hV = new List<string>();
+
+            hN.Add("Jwt-Refresh-Token");
+            hV.Add(Testing.instance.RefreshToken);
+            hN.Add("User-Id");
+            hV.Add(Testing.instance.UserId);
+
+            RequestLogout request = new RequestLogout();
+            request.deviceToken = "testingtesting";
+
+            Testing.instance.Webbing("access-token", "POST", request, (uwr) => {
+                Response<string> response = JsonUtility.FromJson<Response<string>>(uwr.downloadHandler.text);
+                if (response.code == 1000)
+                {
+                    Debug.Log(response.result);
+                    Testing.instance.AccessToken = uwr.GetResponseHeader("Jwt-Access-Token");
+                    Testing.instance.RefreshToken = uwr.GetResponseHeader("Jwt-Refresh-Token");
+                    Debug.Log(uwr.GetResponseHeader("Jwt-Access-Token"));
+                    Debug.Log(uwr.GetResponseHeader("Jwt-Refresh-Token"));
+                }
+                else
+                {
+                    Debug.Log(response.message);
+                }
+            }, hN, hV);
+        });
+
+        SetBtn((int)Buttons.Login_btn, (data) => {
+
+            RequestLogin request = new RequestLogin
+            {
+                email = "tester1@gmail.com",
+                deviceToken = "testingtesting",
+                password = "test12345"
+            };
+
+            Testing.instance.Webbing("login", "POST", request, (uwr) => {
+                Response<RequestTest> response = JsonUtility.FromJson<Response<RequestTest>>(uwr.downloadHandler.text);
+                if (response.code == 1000)
+                {
+                    Debug.Log(response.result);
+                    Testing.instance.AccessToken = uwr.GetResponseHeader("Jwt-Access-Token");
+                    Testing.instance.RefreshToken = uwr.GetResponseHeader("Jwt-Refresh-Token");
+                    Testing.instance.UserId = response.result.userId.ToString();
+                }
+                else
+                {
+                    Debug.Log(response.message);
+                }
+            }, null, null);
+        });
     }
+
 
     private void Start()
     {
