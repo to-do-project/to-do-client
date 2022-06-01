fd5051507ca67f361a0591aae6a048900506ba61
remice <qortmdwns3@gmail.com>
2022-06-01 23:01:24 +0900

    리소스 추가 및 테스트 코드 삭제
    아이템 스토어에 사용할 이미지 추가
    이미지를 적용하기 위한 코드 작성 ItemImageContainer
    테스트 코드 Testing 스크립트 삭제

---
+++ Assets/Scripts/UI/ScrollContents/ItemImageContainer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemImageContainer : MonoBehaviour
+{
+    public int CharGap;
+    public Sprite[] CharItemSprites;
+    public int PlanetGap;
+    public Sprite[] PlanetItemSprites;
+}
