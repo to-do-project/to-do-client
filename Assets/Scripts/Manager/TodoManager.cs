63216b9e1a14a1ce73e572ac122374d737c34d6f
hongmon1 <hongmon1@naver.com>
2022-06-12 00:38:58 +0900

    todo API 연결

--- Assets/Scripts/Manager/TodoManager.cs
+++ Assets/Scripts/Manager/TodoManager.cs
@@ -1,17 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 
 //API response request class 만들기
+public class ResponseMainTodo
+{
+    public long goalId;
+    public string goalTitle;
+    public string groupFlag;
+    public int percentage;
+    public string memberRole;
+    public List<TodoItem> getIdoMainResList;
+}
+
+public class TodoItem
+{
+    public long todoMemberId;
+    public string todoTitle;
+    public string completeFlag;
+    public int likeCount;
+    public int likeFlag;
+}
 
 public class TodoManager
 {
-    List<string> goalList; //목표 리스트
+    public List<ResponseMainTodo> goalList; //목표 리스트
     
+    Response<ResponseMainTodo> res;
+    Action innerAction;
+    Action<UnityWebRequest> callback;
+
+
+
     public void Init()
     {
 
+
     }
+
+
+    public void SendMainGoalRequest()
+    {
+        res = new Response<ResponseMainTodo>();
+
+        //Managers.Web.SendGetRequest("api/goals",)
+    }
+
+    
 
 }
