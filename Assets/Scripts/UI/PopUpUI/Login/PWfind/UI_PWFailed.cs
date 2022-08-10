7df465b98952c612f1c277ef270b444331aca5f9
hongmon1 <hongmon1@naver.com>
2022-08-10 23:40:57 +0900

    UI 수정

---
+++ Assets/Scripts/UI/PopUpUI/Login/PWfind/UI_PWFailed.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_PWFailed : UI_PWfind
+{
+    enum Buttons
+    {
+        Back_btn,
+        login_btn,
+    }
+
+    public override void Init()
+    {
+        base.Init();
+
+        Bind<Button>(typeof(Buttons));
+
+        GameObject backBtn = GetButton((int)Buttons.Back_btn).gameObject;
+        BindEvent(backBtn, ClosePopupUI, Define.TouchEvent.Touch);
+
+        GameObject loginBtn = GetButton((int)Buttons.login_btn).gameObject;
+        BindEvent(loginBtn, loginBtnClick, Define.TouchEvent.Touch);
+    }
+
+    void Start()
+    {
+        Init();
+    }
+
+    private void loginBtnClick(PointerEventData data)
+    {
+        Managers.Sound.PlayNormalButtonClickSound();
+        Managers.UI.ShowPanelUI<UI_Login>("LoginView");
+    }
+}
